5a9e7cff999f645f36b73b8322da4f5ef06b17c3
刘锦洋 <779542115@qq.com>
2019-02-23 17:36:50 +0800

    dio提交

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,14 @@
+const httpHeaders = {
+  "Accept": "application/json, text/plain, */*",
+  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Language": "zh-CN,zh;q=0.9",
+  "Cache-Control": "no-cache",
+  "Connection": "keep-alive",
+  "Content-Type": "application/json",
+  "Cookie":"Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1550912055; _ga=GA1.2.599208789.1550912055; _gid=GA1.2.1449256427.1550912055; _gat=1; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1550912602|1550912052; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1550912605",
+  "Host": "time.geekbang.org",
+  "Origin": "https://time.geekbang.org",
+  "Pragma": "no-cache",
+  "Referer": "https://time.geekbang.org/",
+  "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import "package:dio/dio.dart";
+import "package:flutter_shop/config/httpHeaders.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String showText = '没有数据';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(
-        child:Text('首页')
-      )
-    );
+    return Container(
+        child: Scaffold(
+      appBar: AppBar(title: Text("请求数据")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+                onPressed: () {
+                  _jike();
+                },
+                child: Text('请求数据')),
+            Text('$showText')
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void _jike() {
+    print('开始向极客时间请求数据');
+
+    getHttp().then((val) {
+      setState(() {
+        showText = val['data'].toString();
+      });
+    });
+  }
+
+  Future getHttp() async {
+    try {
+      Response response;
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      print(httpHeaders);
+      response =
+          await dio.get("https://time.geekbang.org/serv/v1/column/newAll");
+      print(response);
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
