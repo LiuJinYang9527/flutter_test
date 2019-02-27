a990b9883f5d1e27d887f49875f6c0a39dca3ac2
刘锦洋 <779542115@qq.com>
2019-02-27 21:36:46 +0800

    导航

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "./pages/index_page.dart";
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,12 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         child: (MaterialApp(
-      title: 'test',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.pink
-      ),
-      home:IndexPage()
-    )));
+            title: 'test',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(primaryColor: Colors.pink),
+            home: IndexPage())));
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,8 +37,12 @@ class _HomePageState extends State<HomePage> {
               if (snapshot.hasData) {
                 var data = json.decode(snapshot.data.toString());
                 List<Map> swiper = (data['data']['slides'] as List).cast();
+                List<Map> navigatorList = (data['data']['category'] as List).cast();
                 return Column(
-                  children: <Widget>[SwiperDiy(swiperDataList: swiper)],
+                  children: <Widget>[
+                    SwiperDiy(swiperDataList: swiper),
+                    TopNavigator(navigatorList: navigatorList)
+                  ],
                 );
               } else {
                 return Center(child: Text('加载中。。。'));
@@ -57,7 +61,6 @@ class SwiperDiy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     print('设备像素密度${ScreenUtil.pixelRatio}');
     print('设备的高${ScreenUtil.screenHeight}');
     print('设备的宽${ScreenUtil.screenWidth}');
@@ -73,6 +76,45 @@ class SwiperDiy extends StatelessWidget {
         pagination: SwiperPagination(),
         autoplay: true,
       ),
+    );
+  }
+}
+
+//导航布局
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+        print('点击了导航');
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'], width: ScreenUtil().setWidth(95)),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(this.navigatorList.length>10){
+      this.navigatorList.removeRange(10, navigatorList.length);
+    }
+    return Container(
+      height:ScreenUtil().setHeight(320),
+      padding:EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding:EdgeInsets.all(5.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
+      )
     );
   }
 }
