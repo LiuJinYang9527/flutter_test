92a71a0509f8faf624bbfb02a0abb42ad3a75a92
LiuJinYang9527 <779542112@qq.com>
2019-05-06 22:48:32 +0800

    增加shared_preferences

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,45 +1,72 @@
 import "package:flutter/material.dart";
-import "package:provide/provide.dart";
-import "../provide/counter.dart";
-class CartPage extends StatelessWidget {
+import "package:shared_preferences/shared_preferences.dart";
+
+class CartPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Column(
-      children: <Widget>[
-        Number(),
-        MyButton()
-      ],
-    )));
-  }
+  _CartPageState createState() => _CartPageState();
 }
 
-class Number extends StatelessWidget {
+class _CartPageState extends State<CartPage> {
+  List<String> testList = [];
   @override
   Widget build(BuildContext context) {
+    _show();
     return Container(
-            margin: EdgeInsets.only(top: 80),
-      child: Provide<Counter>(
-        builder: (context,child,counter){
-          return Text('${counter.value1}',style: Theme.of(context).textTheme.display1);
-        },
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 500,
+            child: ListView.builder(
+                itemCount: testList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${index}'),
+                  );
+                }),
+          ),
+          RaisedButton(
+            onPressed: () {
+              _add();
+            },
+            child: Text('增加'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              _clear();
+            },
+            child: Text('删除'),
+          ),
+        ],
       ),
     );
   }
-}
 
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 80),
-      child: RaisedButton(
-        onPressed: () {
-          Provide.value<Counter>(context).increment();
-        },
-        child: Text('自增'),
-      ),
-    );
+  //增加方法
+  void _add() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp = "123";
+    testList.add(temp);
+    prefs.setStringList("testInfo", testList);
+    _show();
+  }
+
+  //查询
+  void _show() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getStringList('testInfo') != null) {
+      setState(() {
+        testList = prefs.getStringList('testInfo');
+      });
+    } else {}
+  }
+
+  //删除
+  void _clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.clear();
+    prefs.remove('testInfo');
+    setState(() {
+      testList = [];
+    });
   }
 }
